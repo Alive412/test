da3203113ef9c8590ed918e233514a967e3c44d8
Alive412 <oblivion412000@gmail.com>
2025-01-16 14:16:55 +0200

    Update st.py

--- st.py
+++ st.py
@@ -17,6 +17,6 @@ class VideoProcessor:
 
 webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
 				rtc_configuration=RTCConfiguration(
-					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+					{"iceServers": [{"urls": ["stun:stun4.l.google.com:19302"]}]}
 					)
 	)
